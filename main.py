c592596886d52489573d82bbb7138542530060ae
Maklai <maklai26rus@mail.ru>
2022-04-12 07:50:29 +0300

    точка сохранения. Перехожу к тестам

--- main.py
+++ main.py
@@ -21,19 +21,18 @@ def run(message):
     :return:
     """
     if message.text == '/start':
-
-        start_main(message)
-    elif message.text == '/hospital':
-        phone_processing(message)
-    elif message.text == '/polyclinic':
-        consulting_diagnostic_center(message)
+        start_menu(message)
+    # elif message.text == '/hospital':
+    #     phone_processing(message)
+    # elif message.text == '/polyclinic':
+    #     consulting_diagnostic_center(message)
     elif message.text == '/location':
         get_location(message)
     else:
         bot.send_message(message.chat.id, TEXT.main_unit['ERROR'])
 
 
-def start_main(message):
+def start_menu(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.add(
         telebot.types.InlineKeyboardButton(text=TEXT.main_unit['phones_menu'], callback_data='/phones'))
@@ -45,13 +44,18 @@ def start_main(message):
 
 @bot.message_handler(content_types=['text'])
 def message_processing(message):
+    """
+    Функци отлавливание сообщений от пользователя
+    :param message:
+    :return:
+    """
     bot.send_message(message.chat.id, TEXT.hospital['ERROR'])
 
 
 @bot.callback_query_handler(func=lambda call: call.data == '/phones')
-def callback(call):
+def callback_phones(call):
     """
-    Отпработка нажития команды
+    Отпработка нажития команды /phones
     :param call:
     :return:
     """
@@ -64,7 +68,6 @@ def callback(call):
         keyboard.add(
             telebot.types.InlineKeyboardButton(text=TEXT.main_unit['polyclinic'],
                                                callback_data='/polyclinic'))
-        # keyboard.add(telebot.types.InlineKeyboardButton(text=TEXT.main_unit['location'], callback_data='/location'))
 
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                               text=f"*{TEXT.main_unit['phones']}*", parse_mode="Markdown",
@@ -72,9 +75,14 @@ def callback(call):
 
 
 @bot.callback_query_handler(func=lambda call: call.data in PHONES_COM)
-def callback2(call):
+def callback_phones_com(call):
+    """
+    Отпработка нажития команд PHONES_COM
+    :param call:
+    :return:
+    """
     keyboard = telebot.types.InlineKeyboardMarkup()
-    # dept = []
+
     if call.data == "/hospital":
         TEXT.dept = [v for v in TEXT.hospital['Телефонная книга']]
     elif call.data == "/polyclinic":
@@ -118,14 +126,27 @@ def callback2(call):
 
 
 @bot.callback_query_handler(func=lambda call: call.data == '/location')
-def callback5(call):
+def callback_location(call):
+    """
+    Локация
+    :param call:
+    :return:
+    """
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"*{TEXT.main_unit['LOCATION']}*", parse_mode="Markdown")
     bot.send_location(call.message.chat.id, latitude=TEXT.main_unit['latitude'], longitude=TEXT.main_unit['longitude'])
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback6(call):
+def callback_true(call):
+    """
+    Ловим последний калбек для отображения данных
+    :param call:
+    :return:
+    """
+    # if call.data == '/menu':
+    #     start_menu(call.message)
+
     if call.data in TEXT.hospital['Телефонная книга']:
         _phone_dict = TEXT.hospital['Телефонная книга'][call.data]['Телефон']
     elif call.data in TEXT.polyclinic['Телефонная книга']:
@@ -141,49 +162,32 @@ def callback6(call):
                           text=f"{t}")
 
 
-def method_name():
-    inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
-    # inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
-    inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_1)
-    inline_kb_full.add(InlineKeyboardButton('Вторая кнопка', callback_data='btn2'))
-    inline_btn_3 = InlineKeyboardButton('кнопка 3', callback_data='btn3')
-    inline_btn_4 = InlineKeyboardButton('кнопка 4', callback_data='btn4')
-    inline_btn_5 = InlineKeyboardButton('кнопка 5', callback_data='btn5')
-    inline_kb_full.add(inline_btn_3, inline_btn_4, inline_btn_5)
-    inline_kb_full.row(inline_btn_3, inline_btn_4, inline_btn_5)
-    # inline_kb_full.insert(InlineKeyboardButton("query=''", switch_inline_query=''))
-    # inline_kb_full.insert(InlineKeyboardButton("query='qwerty'", switch_inline_query='qwerty'))
-    # inline_kb_full.insert(InlineKeyboardButton("Inline в этом же чате", switch_inline_query_current_chat='wasd'))
-    inline_kb_full.add(
-        InlineKeyboardButton('Уроки aiogram', url='https://surik00.gitbooks.io/aiogram-lessons/content/'))
-
-
-def phone_processing(message):
-    """
-    Обработка телефоного справочника hospital.json
-    :param message:
-    :return:
-    """
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    [keyboard.add(telebot.types.InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
-     TEXT.hospital['Телефонная книга']]
-    bot.send_message(message.chat.id,
-                     f"Выбедите нужное отделение \n",
-                     reply_markup=keyboard)
-
-
-def consulting_diagnostic_center(message):
-    """
-    Обработка телефоного справочника consulting_diagnostic.json
-    :param message:
-    :return:
-    """
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    [keyboard.add(telebot.types.InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
-     TEXT.polyclinic['Телефонная книга']]
-    bot.send_message(message.chat.id,
-                     f"Выбедите нужное отделение \n",
-                     reply_markup=keyboard)
+# def phone_processing(message):
+#     """
+#     Обработка телефоного справочника hospital.json
+#     :param message:
+#     :return:
+#     """
+#     keyboard = telebot.types.InlineKeyboardMarkup()
+#     [keyboard.add(telebot.types.InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
+#      TEXT.hospital['Телефонная книга']]
+#     bot.send_message(message.chat.id,
+#                      f"Выбедите нужное отделение \n",
+#                      reply_markup=keyboard)
+#
+#
+# def consulting_diagnostic_center(message):
+#     """
+#     Обработка телефоного справочника consulting_diagnostic.json
+#     :param message:
+#     :return:
+#     """
+#     keyboard = telebot.types.InlineKeyboardMarkup()
+#     [keyboard.add(telebot.types.InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
+#      TEXT.polyclinic['Телефонная книга']]
+#     bot.send_message(message.chat.id,
+#                      f"Выбедите нужное отделение \n",
+#                      reply_markup=keyboard)
 
 
 def get_location(message):
