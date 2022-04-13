22a7043df353d32dba64cb4367988b32d292ce65
Maklai <maklai26rus@mail.ru>
2022-04-13 16:10:26 +0300

    Начинаю разработку блока ЗАпись

--- main.py
+++ main.py
@@ -21,21 +21,25 @@ def run(message):
     :return:
     """
     if message.text == '/start':
-        start_menu(message)
+        # start_menu()
+        bot.send_message(message.from_user.id,
+                         f"*{TEXT.main_unit['Добро пожаловать']}*\n",
+                         reply_markup=start_menu(), parse_mode="Markdown")
+
     elif message.text == '/location':
         get_location(message)
+        foot_menu(message)
     else:
         bot.send_message(message.chat.id, TEXT.main_unit['ERROR'])
 
 
-def start_menu(message):
+def start_menu():
     keyboard = InlineKeyboardMarkup()
     keyboard.add(
         InlineKeyboardButton(text=TEXT.main_unit['phones_menu'], callback_data='/phones'))
     keyboard.add(InlineKeyboardButton(text=TEXT.main_unit['address_menu'], callback_data='/location'))
-    bot.send_message(message.from_user.id,
-                     f"*{TEXT.main_unit['Добро пожаловать']}*\n",
-                     reply_markup=keyboard, parse_mode="Markdown")
+    keyboard.add(InlineKeyboardButton(text=TEXT.main_unit['register'], callback_data='/register'))
+    return keyboard
 
 
 @bot.message_handler(content_types=['text'])
@@ -83,11 +87,6 @@ def callback_phones_com(call):
     :param call:
     :return:
     """
-    keyboard = InlineKeyboardMarkup()
-    inline_btn_menu = InlineKeyboardButton(TEXT.main_unit['short'], callback_data='/menu')
-    inline_btn_3 = InlineKeyboardButton('<<', callback_data='<<')
-    inline_btn_4 = InlineKeyboardButton('>>', callback_data='>>')
-
     if call.data == "/hospital":
         TEXT.dept = [v for v in TEXT.hospital['Телефонная книга']]
     elif call.data == "/administration":
@@ -101,6 +100,11 @@ def callback_phones_com(call):
     elif call.data == '<<':
         TEXT.step_0 -= TEXT.step
         TEXT.step_5 -= TEXT.step
+
+    keyboard = InlineKeyboardMarkup()
+    inline_btn_3 = InlineKeyboardButton('<<', callback_data='<<')
+    inline_btn_menu = InlineKeyboardButton(TEXT.main_unit['short'], callback_data='/menu')
+    inline_btn_4 = InlineKeyboardButton('>>', callback_data='>>')
 
     if TEXT.step_0 <= -1:
         [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
@@ -143,7 +147,12 @@ def callback_location(call):
                           text=f"*{TEXT.main_unit['LOCATION']}*", parse_mode="Markdown", )
     bot.send_location(call.message.chat.id, latitude=TEXT.main_unit['latitude'], longitude=TEXT.main_unit['longitude'])
 
-    main_menu(call)
+    foot_menu(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == '/register')
+def get_register(call):
+    bot.send_message(call.message.chat.id, 'Блок в разработке')
 
 
 def get_location(message):
@@ -152,26 +161,21 @@ def get_location(message):
     bot.send_location(message.chat.id, latitude=TEXT.main_unit['latitude'], longitude=TEXT.main_unit['longitude'])
 
 
-def main_menu(call):
+def foot_menu(message):
     """
     Меню выдаваемое в конце диолога
     """
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton(TEXT.main_unit['menu'], callback_data='/menu'))
-    bot.send_message(call.message.chat.id, TEXT.main_unit['Главное меню'], reply_markup=keyboard)
+    bot.send_message(message.chat.id, TEXT.main_unit['Главное меню'], reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == '/menu')
 def callback_menu(call):
     """Обработка команды /menu"""
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(
-        InlineKeyboardButton(text=TEXT.main_unit['phones_menu'], callback_data='/phones'))
-    keyboard.add(InlineKeyboardButton(text=TEXT.main_unit['address_menu'], callback_data='/location'))
-
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"*{TEXT.main_unit['Добро пожаловать']}*", parse_mode="Markdown",
-                          reply_markup=keyboard)
+                          reply_markup=start_menu())
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -189,16 +193,19 @@ def callback_true(call):
     elif call.data in TEXT.administration['Телефонная книга']:
         _phone_dict = TEXT.administration['Телефонная книга'][call.data]['Телефон']
 
-    t = f"{call.data.replace('/', '')}:\n"
-    for p, v in _phone_dict.items():
-        t += f"{p} : {v} \n"
+    try:
+        t = f"{call.data.replace('/', '')}:\n"
+        for p, v in _phone_dict.items():
+            t += f"{p} : {v} \n"
+    except UnboundLocalError as er:
+        print("Ошибка словаря", er)
 
     TEXT.step_0 = 0
     TEXT.step_5 = TEXT.step
 
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"{t}")
-    main_menu(call)
+    foot_menu(call.message)
 
 
 def main():
