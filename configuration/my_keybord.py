f75200e62631476fd03963f4b4fba804d6e5ace9
Maklai <maklai26rus@mail.ru>
2022-04-25 12:09:43 +0300

    Создал Класс. для упрощение

--- configuration/my_keybord.py
+++ configuration/my_keybord.py
@@ -1,127 +1,128 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from configuration.my_settings import TextBot
 
-TEXT = TextBot()
+
+class MyKeyboard:
+
+    def __init__(self):
+        self.text = TextBot()
+        self.left = "<<<"
+        self.right = ">>>"
+        self.centre = self.text.main_unit['short_menu']
+        self.finish_registration = self.text.main_unit['Главное меню']
+
+    def active_menu(self, dept, step_0, step_5):
+        """
+
+        [*************]
+        [*************]
+        [<<][Меню][>>]]
 
 
-def active_menu(dept, step_0, step_5):
-    """
+        step = TEXT.step
 
-    [*************]
-    [*************]
-    [<<][Меню][>>]]
+        Данные которые должны прийти
 
+        if call.data == '>>':
+            TEXT.step_0 += TEXT.step
+            TEXT.step_5 += TEXT.step
+        elif call.data == '<<':
+            TEXT.step_0 -= TEXT.step
+            TEXT.step_5 -= TEXT.step
 
-    step = TEXT.step
+        dept = TEXT.dept = [v for v in TEXT.***['Телефонная книга']]
 
-    Данные которые должны прийти
+        :param dept: Словарь с данными которые нужно будет отобразить спиком в колонне
+        :param step: Сколько элементов надо вывести на экран
+        :param step_0:
+        :param step_5:
+        :return:
+        """
 
-    if call.data == '>>':
-        TEXT.step_0 += TEXT.step
-        TEXT.step_5 += TEXT.step
-    elif call.data == '<<':
-        TEXT.step_0 -= TEXT.step
-        TEXT.step_5 -= TEXT.step
+        step = self.text.step
+        keyboard = InlineKeyboardMarkup()
+        inline_btn_3 = InlineKeyboardButton(self.left, callback_data=self.left)
+        inline_btn_menu = InlineKeyboardButton(self.centre, callback_data='/menu')
+        inline_btn_4 = InlineKeyboardButton(self.right, callback_data=self.right)
 
-    dept = TEXT.dept = [v for v in TEXT.***['Телефонная книга']]
+        if step_0 <= -1:
+            [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
+             dept[0:step]]
 
-    :param dept: Словарь с данными которые нужно будет отобразить спиком в колонне
-    :param step: Сколько элементов надо вывести на экран
-    :param step_0:
-    :param step_5:
-    :return:
-    """
+            keyboard.row(inline_btn_menu, inline_btn_4)
+            return keyboard
 
-    step = TEXT.step
-    keyboard = InlineKeyboardMarkup()
-    inline_btn_3 = InlineKeyboardButton('<<', callback_data='<<')
-    inline_btn_menu = InlineKeyboardButton(TEXT.main_unit['short'], callback_data='/menu')
-    inline_btn_4 = InlineKeyboardButton('>>', callback_data='>>')
+        elif step_5 <= len(dept):
+            [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
+             dept[step_0:step_5]]
 
-    if step_0 <= -1:
-        [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
-         dept[0:step]]
+            keyboard.row(inline_btn_3, inline_btn_menu, inline_btn_4)
+            return keyboard
 
-        keyboard.row(inline_btn_menu, inline_btn_4)
+        elif step_5 >= len(dept):
+            [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
+             dept[len(dept) - step:step_5]]
+
+            keyboard.row(inline_btn_3, inline_btn_menu)
+            return keyboard
+
+    def main_menu(self):
+        """
+        Основоное меню
+        :return:
+        """
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(text=self.text.main_unit['phones_menu'], callback_data='/phones'))
+        keyboard.add(InlineKeyboardButton(text=self.text.main_unit['address_menu'], callback_data='/location'))
+        keyboard.add(InlineKeyboardButton(text=self.text.main_unit['register'], callback_data='/register'))
         return keyboard
 
-    elif step_5 <= len(dept):
-        [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
-         dept[step_0:step_5]]
+    def menu_phones(self):
+        """
+        Меню телефонов
+        выдает список по вывобру нужной котегории
+        :return:
+        """
+        keyboard = InlineKeyboardMarkup()
 
-        keyboard.row(inline_btn_3, inline_btn_menu, inline_btn_4)
+        keyboard.add(
+            InlineKeyboardButton(text=self.text.main_unit['hospital'],
+                                 callback_data='/hospital', ))
+        keyboard.add(
+            InlineKeyboardButton(text=self.text.main_unit['polyclinic'],
+                                 callback_data='/polyclinic'))
+
+        keyboard.add(
+            InlineKeyboardButton(text=self.text.main_unit['administration'],
+                                 callback_data='/administration'))
+
+        keyboard.add(InlineKeyboardButton(self.text.main_unit['menu'], callback_data='/menu'))
         return keyboard
 
-    elif step_5 >= len(dept):
-        [keyboard.add(InlineKeyboardButton(text=str(v).replace('/', ''), callback_data=v)) for v in
-         dept[len(dept) - step:step_5]]
-
-        keyboard.row(inline_btn_3, inline_btn_menu)
+    def menu_foot(self):
+        """
+        Финальное меню
+        Появляется после выбора пользователя
+        :return:
+        """
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(InlineKeyboardButton(self.text.main_unit['menu'], callback_data='/menu'))
         return keyboard
 
+    def telephone_keys(self):
+        """Блокировка клавиатуры
+        при нажатие запрашивает данные отпользователя
+        """
+        keyboard = ReplyKeyboardMarkup(one_time_keyboard=True)
+        keyboard.add(KeyboardButton(text=f"{self.text.main_unit['register_phones']}", request_contact=True))
+        keyboard.add(KeyboardButton(text=f"{self.text.main_unit['text_not']}"))
+        return keyboard
 
-def main_menu():
-    """
-    Основоное меню
-    :return:
-    """
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(
-        InlineKeyboardButton(text=TEXT.main_unit['phones_menu'], callback_data='/phones'))
-    keyboard.add(InlineKeyboardButton(text=TEXT.main_unit['address_menu'], callback_data='/location'))
-    keyboard.add(InlineKeyboardButton(text=TEXT.main_unit['register'], callback_data='/register'))
-    return keyboard
-
-
-def menu_phones():
-    """
-    Меню телефонов
-    выдает список по вывобру нужной котегории
-    :return:
-    """
-    keyboard = InlineKeyboardMarkup()
-
-    keyboard.add(
-        InlineKeyboardButton(text=TEXT.main_unit['hospital'],
-                             callback_data='/hospital', ))
-    keyboard.add(
-        InlineKeyboardButton(text=TEXT.main_unit['polyclinic'],
-                             callback_data='/polyclinic'))
-
-    keyboard.add(
-        InlineKeyboardButton(text=TEXT.main_unit['administration'],
-                             callback_data='/administration'))
-
-    keyboard.add(InlineKeyboardButton(TEXT.main_unit['menu'], callback_data='/menu'))
-    return keyboard
-
-
-def menu_foot():
-    """
-    Финальное меню
-    Появляется после выбора пользователя
-    :return:
-    """
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton(TEXT.main_unit['menu'], callback_data='/menu'))
-    return keyboard
-
-
-def telephone_keys():
-    """Блокировка клавиатуры
-    при нажатие запрашивает данные отпользователя
-    """
-    keyboard = ReplyKeyboardMarkup(one_time_keyboard=True)
-    keyboard.add(KeyboardButton(text=f"{TEXT.main_unit['register_phones']}", request_contact=True))
-    keyboard.add(KeyboardButton(text=f"{TEXT.main_unit['text_not']}"))
-    return keyboard
-
-
-def regions_keys():
-    """Блокировка клавиатуры
-    при нажатие запрашивает данные отпользователя
-    """
-    keyboard = ReplyKeyboardMarkup(one_time_keyboard=True)
-    [keyboard.add(KeyboardButton(text=f"{v}")) for v in TEXT.region['регион']]
-    # keyboard.add(KeyboardButton(text=f"{TEXT.main_unit['text_not']}"))
-    return keyboard
+    def regions_keys(self):
+        """Блокировка клавиатуры
+        при нажатие запрашивает данные отпользователя
+        """
+        keyboard = ReplyKeyboardMarkup(one_time_keyboard=True)
+        [keyboard.add(KeyboardButton(text=f"{v}")) for v in self.text.region['регион']]
+        return keyboard
--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import os
 # import datetime
 
 import telebot
-# from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from configuration.my_settings import TextBot, UserData, get_date
 
 from decouple import config
 
-from configuration.my_keybord import active_menu, main_menu, menu_phones, menu_foot, telephone_keys, regions_keys
+from configuration.my_keybord import MyKeyboard
 
 SECRET_KEY_BOT = config('SECRET_KEY_BOT')
 
 bot = telebot.TeleBot(SECRET_KEY_BOT)
 TEXT = TextBot()
-PHONES_COM = ['/polyclinic', '/hospital', '/administration', '>>', '<<']
+KEYBOARD = MyKeyboard()
+PHONES_COM = ['/polyclinic', '/hospital', '/administration', KEYBOARD.right, KEYBOARD.left]
 USER = UserData()
 
 
@@ -33,7 +33,7 @@ def run(message):
         USER.id = message.chat.id
         bot.send_message(message.from_user.id,
                          f"*{TEXT.main_unit['Добро пожаловать']}*\n",
-                         reply_markup=main_menu(), parse_mode="Markdown")
+                         reply_markup=KEYBOARD.main_menu(), parse_mode="Markdown")
     elif message.text == '/location':
         get_location(message)
         foot_menu(message)
@@ -61,7 +61,7 @@ def callback_phones(call):
 
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"*{TEXT.main_unit['phones']}*", parse_mode="Markdown",
-                          reply_markup=menu_phones())
+                          reply_markup=KEYBOARD.menu_phones())
 
 
 @bot.callback_query_handler(func=lambda call: call.data in PHONES_COM)
@@ -78,14 +78,14 @@ def callback_phones_com(call):
     elif call.data == "/polyclinic":
         TEXT.dept = [v for v in TEXT.polyclinic['Телефонная книга']]
 
-    if call.data == '>>':
+    if call.data == KEYBOARD.right:
         TEXT.step_0 += TEXT.step
         TEXT.step_5 += TEXT.step
-    elif call.data == '<<':
+    elif call.data == KEYBOARD.left:
         TEXT.step_0 -= TEXT.step
         TEXT.step_5 -= TEXT.step
 
-    keyboard = active_menu(dept=TEXT.dept, step_0=TEXT.step_0, step_5=TEXT.step_5)
+    keyboard = KEYBOARD.active_menu(dept=TEXT.dept, step_0=TEXT.step_0, step_5=TEXT.step_5)
 
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"*{TEXT.main_unit['hospital']}*", parse_mode="Markdown",
@@ -109,8 +109,13 @@ def callback_location(call):
 
 @bot.callback_query_handler(func=lambda call: call.data == '/register')
 def get_register(call):
+    """
+    Обработка команнды запись от пользователя
+    :param call:
+    :return:
+    """
     bot.send_message(chat_id=call.message.chat.id, text=f"{TEXT.main_unit['operator']}",
-                     reply_markup=telephone_keys())
+                     reply_markup=KEYBOARD.telephone_keys())
 
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"{TEXT.main_unit['processing']}", parse_mode="Markdown")
@@ -119,18 +124,26 @@ def get_register(call):
 
 
 def data_processing(message):
+    """
+    Заполнение данных на талон
+    :param message:
+    :return:
+    """
     if message.text == TEXT.main_unit['text_not']:
         bot.send_message(message.chat.id, text=TEXT.main_unit['text_no_phones'])
         foot_menu(message)
     else:
-        USER.phone = message.contact.phone_number
-        USER.fio_children = message.chat.first_name
+        foot_menu(message)
+        try:
+            USER.phone = message.contact.phone_number
+        except AttributeError:
+            print(USER.phone)
+        USER.fio_people = message.chat.first_name
         USER.id = message.chat.id
 
         bot.send_message(message.chat.id, f"*{TEXT.main_unit['text_add_direction']}*", parse_mode="Markdown")
         USER.photo = True
         bot.register_next_step_handler(message, direction_processing)
-        foot_menu(message)
 
 
 @bot.message_handler(content_types=['photo', 'text'])
@@ -156,15 +169,28 @@ def direction_processing(message):
                 os.mkdir(f'photo/{message.chat.id}')
 
             bot.send_message(chat_id=message.chat.id, text=f"{TEXT.main_unit['text_region']}",
-                             reply_markup=regions_keys())
+                             reply_markup=KEYBOARD.regions_keys())
             USER.photo = False
+            bot.register_next_step_handler(message, xxx)
 
         except TypeError:
             bot.send_message(message.chat.id, f"{TEXT.main_unit['text_add_direction']}", parse_mode="Markdown")
             bot.register_next_step_handler(message, direction_processing)
 
 
+def xxx(message):
+    USER.direction = message.text
+    print(USER.direction)
+    print(USER.phone)
+    print(USER.fio_people)
+
+
 def get_location(message):
+    """
+    Вызывает карту. с указаными кооринатами
+    :param message:
+    :return:
+    """
     bot.send_message(message.chat.id, f"*{TEXT.main_unit['LOCATION']}*", parse_mode="Markdown")
     bot.send_location(message.chat.id, latitude=TEXT.main_unit['latitude'], longitude=TEXT.main_unit['longitude'])
 
@@ -173,7 +199,8 @@ def foot_menu(message):
     """
     Меню выдаваемое в конце диолога
     """
-    bot.send_message(message.chat.id, TEXT.main_unit['Главное меню'], reply_markup=menu_foot())
+    bot.send_message(message.chat.id, TEXT.main_unit['Главное меню'], reply_markup=KEYBOARD.menu_foot())
+    # bot.send_message(message.chat.id, KEYBOARD.finish_registration, reply_markup=KEYBOARD.menu_foot())
 
 
 @bot.callback_query_handler(func=lambda call: call.data == '/menu')
@@ -181,7 +208,7 @@ def callback_menu(call):
     """Обработка команды /menu"""
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"*{TEXT.main_unit['Добро пожаловать']}*", parse_mode="Markdown",
-                          reply_markup=main_menu())
+                          reply_markup=KEYBOARD.main_menu())
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -212,23 +239,6 @@ def callback_true(call):
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                           text=f"{t}")
     foot_menu(call.message)
-
-
-# @bot.message_handler(content_types=['photo'])
-# def handle_docs_document(message):
-#     file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
-#     downloaded_file = bot.download_file(file_info.file_path)
-#
-#     if os.path.isdir(f'photo/{message.chat.id}'):
-#         src = f'photo/{message.chat.id}/{get_date()}' + '.' + \
-#               file_info.file_path.split('.')[1]
-#         # src = f'photo/{message.chat.id}/' + file_info.file_path.replace('photos/', '')
-#         with open(src, 'wb') as new_file:
-#             new_file.write(downloaded_file)
-#         USER.photo = True
-#         bot.reply_to(message, "Фото добавлено")
-#     else:
-#         os.mkdir(f'photo/{message.chat.id}')
 
 
 def main():
